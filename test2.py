92f0bf63d7cfc8397c2a0bb7e8c0123d757eede8
Sean Jones <38304815+SeanAJ@users.noreply.github.com>
2024-08-16 19:01:37 -0700

    added home quardinates and convert them

--- test2.py
+++ test2.py
@@ -10,6 +10,20 @@ import numpy as np
 #worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
 #world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
+hlon = 48.4284
+hlat = -123.3656
+
+#convert home location
+hlat, hlon = np.deg2rad(hlat), np.deg2rad(hlon)
+R = 6371 # radius of the earth
+x_h = R * np.cos(hlat) * np.cos(hlon)
+y_h = R * np.cos(hlat) * np.sin(hlon)
+z_h = R *np.sin(hlat)
+x_h = round(x_h,2)
+y_h = round(y_h,2)
+z_h = round(z_h,2)
+print(f"home x: {x_h}, home y: {y_h}, home z: {z_h}")
+
 iteration = 0
 while True:
     response = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
@@ -18,6 +32,9 @@ while True:
     lon = float(data['longitude'])
     lat = float(data['latitude'])
     alt = float(data['altitude'])
+    #lon = float(input("longitude:"))
+    #lat = float(input("latitude: "))
+    #alt = 0
     print(f"longitude: {lon}")
     print(f"latitude: {lat}")
     print(f"altitude: {alt}")
@@ -30,7 +47,10 @@ while True:
     x = round(x,2)
     y = round(y,2)
     z = round(z,2)
-    print(f"x: {x}, y: {y}, z:{z}")
+    print(f"x: {x}")
+    print(f"y: {y}")
+    print(f"z: {z}")
+
     iteration += 1
     print (iteration)
     if iteration == 1:
