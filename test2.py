cb77a253818ac43fc5e7b668f9efceb0ba44f40e
Sean Jones <38304815+SeanAJ@users.noreply.github.com>
2024-08-18 15:44:36 -0700

    fixed the radius of the earth which was causing and incorrect distance to the nadir

--- test2.py
+++ test2.py
@@ -20,10 +20,7 @@ R = 6371 # radius of the earth
 x_h = R * np.cos(hlat) * np.cos(hlon)
 y_h = R * np.cos(hlat) * np.sin(hlon)
 z_h = R *np.sin(hlat)
-x_h = round(x_h,2)
-y_h = round(y_h,2)
-z_h = round(z_h,2)
-print(f"home x: {x_h}, home y: {y_h}, home z: {z_h}")
+print(f"home x: {round(x_h, 2)}, home y: {round(y_h, 2)}, home z: {round(z_h, 2)}")
 
 iteration = 0
 while True:
@@ -41,16 +38,13 @@ while True:
     print(f"altitude: {alt}")
 
     lat, lon = np.deg2rad(lat), np.deg2rad(lon)
-    R = 6371 + alt # radius of the earth
+    R = 6371 # radius of the earth
     x = R * np.cos(lat) * np.cos(lon)
     y = R * np.cos(lat) * np.sin(lon)
     z = R *np.sin(lat)
-    x = round(x,2)
-    y = round(y,2)
-    z = round(z,2)
-    print(f"x: {x}")
-    print(f"y: {y}")
-    print(f"z: {z}")
+    print(f"x: {round(x, 2)}")
+    print(f"y: {round(y, 2)}")
+    print(f"z: {round(z, 2)}")
 
     X = np.cos(lat) * np.sin(lon-hlon)
     Y = np.cos(hlat) * np.sin(lat) - np.sin(hlat) * np.cos(lat) * np.cos(lon-hlon)
@@ -59,7 +53,7 @@ while True:
 
     print(f"B: {B}")
     #Calculate the distance from the observer to the nadir of the iss
-    ON = np.sqrt(((x - x_h)**2) + ((y- y_h)**2) + ((z - z_h)**2))
+    ON = np.sqrt(((x - x_h)**2) + ((y - y_h)**2) + ((z - z_h)**2))
     print(f"nadir: {ON}")
     # Calculate the geocentric angle
     GA = 2 * math.asin(ON/(2 * R))
@@ -72,7 +66,6 @@ while True:
     OAD = np.rad2deg(OA)
     print(f"angle  from observer to iss: {OAD}")
     
-
     iteration += 1
     print (iteration)
     if iteration == 1:
